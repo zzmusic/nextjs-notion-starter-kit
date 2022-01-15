20c82377f490a5a3effa81c89e05ef8f2e817d98
zzmusic <70708798+zzmusic@users.noreply.github.com>
2022-01-15 14:33:30 +0000

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'caacd369538d40e0b83c435d796b214e',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Castalia',
+  domain: 'https://www.notion.so/castalia-april/April-s-Personal-Website-caacd369538d40e0b83c435d796b214e',
+  author: 'April Wu',
 
   // open graph metadata (optional)
   description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
+  socialImageTitle: 'April Wu - Castalia',
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  Instagram: 'thechildofthestars',
+  Facebook: 'April Wu',
+  linkedin: 'April Wu',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
